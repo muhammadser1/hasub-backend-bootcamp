92512b050273d76d0b861cb77ee42ad24ff0dfc3
muhammad_Sarahni <117571362+muhammadser1@users.noreply.github.com>
2024-03-15 03:00:19 +0200

    truck2

--- truck/main.py
+++
@@ -1,8 +0,0 @@
-from handler.JSONHandler import load_data_from_json
-from simulation_engine.Simulation import Simulation
-from utils.road_utils import load_roads
-
-road_data = load_data_from_json("data_files/roads.json")
-roads = load_roads(road_data["roads"])
-simulation = Simulation(roads)
-simulation.start()
--- truck/simulation_engine/Road.py
+++
@@ -1,9 +0,0 @@
-class Road:
-    def __init__(self, name, fuel_consumption, mental_effect, wheel_damage_effect,length):
-        self.name = name
-        self.fuel_consumption = fuel_consumption
-        self.mental_effect = mental_effect
-        self.wheel_damage_effect = wheel_damage_effect
-        self.length=length
-    def __str__(self):
-        return f"Road name: {self.name}, Fuel Consumption: {self.fuel_consumption}, Mental Effect: {self.mental_effect}, Wheel Damage Effect: {self.wheel_damage_effect}, Length: {self.length}"
